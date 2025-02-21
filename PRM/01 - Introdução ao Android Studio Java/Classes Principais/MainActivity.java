fe40d723ab2fb2456bc841a998e7853592b25aa7
RRZamboni <rafael.zamboni@etip.santoandre.br>
2025-02-21 10:54:34 -0300

    Update MainActivity.java

--- PRM/01 - Introdução ao Android Studio Java/Classes Principais/MainActivity.java
+++ PRM/01 - Introdução ao Android Studio Java/Classes Principais/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     TextView titulo;
     Button btnTestar;
     TextView resposta;
+    EditText txtNome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
         titulo = (TextView) findViewById(R.id.Titulo);
         btnTestar = (Button) findViewById(R.id.BtnTestar);
         resposta = (TextView) findViewById(R.id.Resposta);
+        txtNome = (EditText) findViewById(R.id.txtNome);
 
         // 3) Programando o evento do botão
         btnTestar.setOnClickListener(new View.OnClickListener()
@@ -29,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
-                resposta.setText("Olá queridão, acho que te amo!");
+                //Recuperando valor
+                String nome = txtNome.getText().toString();
+
+                resposta.setText("Olá queridão " +nome +", acho que te amo!");
             }
         });//btnTestar
     }
