c350a83a9497e76afb7d1cefa13a5592a93e2313
RRZamboni <rafael.zamboni@etip.santoandre.br>
2025-02-21 10:16:31 -0300

    Update MainActivity.java

--- PRM/01 - Introdução ao Android Studio Java/Classes Principais/MainActivity.java
+++ PRM/01 - Introdução ao Android Studio Java/Classes Principais/MainActivity.java
@@ -3,25 +3,34 @@ package com.example.meuprojetinhochuchu;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
     // 1) Atributos
     TextView titulo;
     Button btnTestar;
     TextView resposta;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         // 2) 'Linkando' os itens da tela com os atributos
         titulo = (TextView) findViewById(R.id.Titulo);
+        btnTestar = (Button) findViewById(R.id.BtnTestar);
+        resposta = (TextView) findViewById(R.id.Resposta);
 
-
+        // 3) Programando o evento do botão
+        btnTestar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                resposta.setText("Olá queridão, acho que te amo!");
+            }
+        });//btnTestar
     }
 }
