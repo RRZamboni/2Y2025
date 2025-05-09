8ac30f9331c2bebb739f19a5233bd79d6c96d866
RRZamboni <rafael.zamboni@etip.santoandre.br>
2025-05-09 10:50:05 -0300

    Update MainActivity.java

--- PRM/06_Aula_ArrayList/ClassesPrincipais/MainActivity.java
+++ PRM/06_Aula_ArrayList/ClassesPrincipais/MainActivity.java
@@ -21,6 +21,9 @@ public class MainActivity extends AppCompatActivity {
     ImageView imagem;
     Spinner spinner;
 
+    int img;
+    String txtArt;
+
 
 
     @Override
@@ -40,20 +43,23 @@ public class MainActivity extends AppCompatActivity {
              de vários tipos, além de métodos de acesso!!!
          */
         ArrayList<String> listinha = new ArrayList<String>();
+        listinha.add("");
         listinha.add("Black Sabbath");
-        listinha.add("Halloween");
-        listinha.add("Mc Mento");
-        listinha.add("Angra");
-        listinha.add("Iron Maiden");
+        listinha.add("Helloween");
+        listinha.add("Kiss");
+        listinha.add("Rush");
+        listinha.add("Tralalero");
 
         // 4) Adaptando o ArrayList<> para colocar no layout
         ArrayAdapter<String> adaptador =
                 new ArrayAdapter<>(MainActivity.this,
-                         android.R.layout.simple_list_item_1,
-                          listinha);
+                        android.R.layout.simple_list_item_1,
+                        listinha);
 
         // 5) Inserindo a Lista Adaptada no layout
         spinner.setAdapter(adaptador);
+
+        ////////////////////////////////////////////////////////
 
 
         // 6) Evento no Spinner
@@ -62,11 +68,53 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
             {
+
+                //Recuperando valor selecionado
                 String itemSelecionado = listinha.get(i);
 
-                Toast.makeText(MainActivity.this,
-                                itemSelecionado,
-                        Toast.LENGTH_LONG).show();
+                //Verificando campos
+                if(itemSelecionado.equals(""))
+                {
+                    //Do nothing!!!! MOtherFocker!!!
+                    imagem.setImageResource(R.drawable.ic_launcher_background);
+                }
+
+                if(itemSelecionado.equals("Black Sabbath"))
+                {
+                    //Coloco os valores nas variaveis
+                    img = R.drawable.bs;
+                    txtArt = "Black Sabbath é uma banda de heavy metal britânica formada no ano de 1968 em Birmingham.";
+                }
+
+                if(itemSelecionado.equals("Helloween"))
+                {
+                    //Coloco os valores nas variaveis
+                    img = R.drawable.helo;
+                    txtArt = "Helloween é uma influente banda de power metal alemã, fundada em 1984. É composta por Andi Deris (vocal), Dani Löble (bateria), Sascha Gerstner (guitarra), Michael Weikath (guitarra).";
+                }
+
+                if(itemSelecionado.equals("Kiss"))
+                {
+                    //Coloco os valores nas variaveis
+                    img = R.drawable.kiss;
+                    txtArt = "Kiss foi uma banda de hard rock dos Estados Unidos, formada em Nova Iorque em 1973 por Paul Stanley e Gene Simmons. A banda passou por algumas mudanças de formações, sendo Stanley e Simmons os dois integrantes da formação original da banda a participarem de todas elas.";
+                }
+
+                if(itemSelecionado.equals("Rush"))
+                {
+                    img = R.drawable.rush;
+                    txtArt = "Rush foi uma banda formada em 1968 em Toronto, Ontário, que se destacou pela habilidade musical, composições complexas e letras ecléticas. ";
+                }
+
+                if(itemSelecionado.equals("Tralalero"))
+                {
+                    img = R.drawable.tralalero;
+                    txtArt = " Tralalero Tralalá, Bombardiro Crocodilo, Brr Brr Patapim, Tung Tung Tung Tung Sahur.";
+                }
+
+                //Modificando os valores dos elementos do layout
+                imagem.setImageResource(img);
+                textoArtista.setText(txtArt);
             }
 
             @Override
